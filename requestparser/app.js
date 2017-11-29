32b4c5b4c9931edaea6489dc5d154ffbd219f516
jon <jrinciari@or-specialties.com>
2017-11-29 20:42:49 +0000

    added os module

--- requestparser/app.js
+++ requestparser/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,9 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 
+app.use('/', index);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -45,7 +43,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-app.listen(process.env.PORT, process.env.IP, function(req, res){
-  console.log('server on')
-})
--- requestparser/routes/index.js
+++ requestparser/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(),
+    accepts = require('accepts'),
+    os = require('os')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Request Header Parser Microservice' });
 });
 
+
+router.get('/whoami', function(req, res){
+    var ip = req.ip;
+    var lang = accepts(req).language();
+    var system = `${os.type()}; ${os.platform()}, ${os.release()}`
+    res.send({
+      ipaddress: ip,
+      language: lang[0], 
+      Software: system 
+    })
+})
 module.exports = router;
